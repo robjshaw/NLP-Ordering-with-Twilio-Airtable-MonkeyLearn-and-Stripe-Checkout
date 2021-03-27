8ce60c79cdfa50a8b9a8db99b1fb537ea960b177
Rob Shaw <rshaw@twilio.com>
2021-03-28 10:38:10 +1100

    added ML extraction into it.

--- functions/inbound.js
+++ functions/inbound.js
@@ -1,9 +1,78 @@
 exports.handler = function(context, event, callback) {
 
-    // we need to handle event.BODY
+    const MonkeyLearn = require('monkeylearn')
+    
+    var Airtable = require('airtable');
+    var base = new Airtable({apiKey: process.env.AIRTABLEKEY}).base(process.env.AIRTABLEBASE);
 
-    var response = 'Hi welcome to ' + process.env.PLACE + ' check out our menu ' + process.env.HOST + '/menu.html?table=43&location=1'
+    var inboundmessage = event.Body.toLowerCase();
+    
+    if (inboundmessage.includes('menu') == true){
 
-    callback(null, response);
+        console.log('menu');
+
+        // TODO extract table number & short url
+
+        var response = 'Hi welcome to ' + process.env.PLACE + ' check out our menu ' + process.env.HOST + '/menu.html?table=43&location=1'
+
+        callback(null, response);
+
+    }else{
+
+        console.log('order');
+
+        const ml = new MonkeyLearn(process.env.MLKEY)
+        let model_id = process.env.MLMODEL
+        let data = [inboundmessage]
+
+        var order = [];
+
+        ml.extractors.extract(model_id, data).then(res => {
+            console.log(null, res.body);
+
+            var response = res.body[0];
+
+            response = response.extractions;
+
+            var currentorder = 0;
+
+            response.forEach(function (arrayItem) {
+                var type = arrayItem.tag_name;
+
+                switch(type) {
+                    case 'quantity':
+
+                        var qty = 0;
+
+                        if (arrayItem.parsed_value == 'a'){
+                            qty = 1;
+                        }else{
+                            qty = parseInt(parsed_value);
+                        }
+
+                        console.log(qty);
+
+                        break;
+
+                    case 'product':
+
+                        console.log('product1');
+                    
+                        break;
+
+                    case 'drink':
+
+                        console.log('drink1');
+                        
+                        
+                        break;
+
+                }
+            });
+
+            callback(null, response);
+        })
+
+    }
 
 }
